d631af3bb52ed6f8745797da85f9980148fd1b8e
JairusSW <jairus.v.tanaka@outlook.com>
2021-07-15 08:50:59 -0700

    v0.6.1

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ const fromArray = (data) => {
   let result = "[";
   const lastChunk = data[data.length - 1];
   let chunk;
-  let i = 0;
-  for (; i < data.length - 1; i++) {
+  let i = 0 | 0;
+  for (; i < (data.length - 1) | 0; i++) {
     chunk = data[i];
     result += `${stringify(chunk)},`;
   }
@@ -37,13 +37,13 @@ const fromArray = (data) => {
 
 const fromObject = (data) => {
   const keys = Object.keys(data);
-  if (keys.length === 0) {
+  if (keys.length | 0 === 0 | 0) {
     return "{}";
   }
   let result = "{";
-  const lastKey = keys[keys.length - 1];
+  const lastKey = keys[(keys.length - 1) | 0];
   let key;
-  for (let i = 0; i < keys.length - 1; i++) {
+  for (let i = 0 | 0; i < (keys.length - 1) | 0; i++) {
     key = keys[i]
     result += `${stringify(key)}:${stringify(data[key])},`;
   }
@@ -76,14 +76,14 @@ function isArrayish(obj) {
   return (
     obj instanceof Array ||
     Array.isArray(obj) ||
-    (obj.length >= 0 && obj.splice instanceof Function)
+    ((obj.length) | 0 >= 0 | 0 && obj.splice instanceof Function)
   );
 }
 
 // Parse
 
 const toString = (data) => {
-  return data.slice(1, data.length - 1).replace(escapedToQuoteRegex, '"');
+  return data.slice(1 | 0, (data.length - 1) | 0).trim().replace(escapedToQuoteRegex, '"');
 };
 
 const toNumber = (data) => {
@@ -91,48 +91,44 @@ const toNumber = (data) => {
 };
 
 const toBoolean = (data) => {
-  if (data[0] == "t") return true;
+  if (data[0 | 0] == "t") return true;
   return false;
 };
 
 const toArray = (data) => {
-  console.log("Data: ", data);
-  let pos = 0;
-  // 0 = Normal
-  // 1 = Subarray
-  const result = [];
-  let lastPos = 0;
-  let depth = 0;
-  let foundDepth = 0;
-  let chunk;
-  for (let i = 0; i < data.length; i++) {
-    chunk = data[i];
-    if (chunk === "[") depth++;
-    if (chunk === "]") foundDepth++;
-    if (pos === 0 && chunk === "[") {
-      pos = 1;
-      lastPos = i;
+  let result = []
+  let lastPos = -1 | 0;
+  let char;
+  let depth = 0 | 0;
+  let fdepth = 0 | 0;
+  let instr = false;
+  for (let i = 0 | 0; i < (data.length - 1) | 0; i++) {
+    char = data[i];
+    // This ignores [ and ] if they are inside a string.
+    if (data[(i - 1) | 0] != "\\" && char == "\"")
+      instr = (instr ? false : true);
+    // This gets the depth of the array/object
+    if (instr === false && (char === "[" || char === "{")) depth++;
+    if (instr === false && (char === "]" || char === "}")) fdepth++;
+    // If the depth and found depth are equal, that is an array. Push it.
+    if (instr === false && depth > 0 && depth === fdepth) {
+      result.push(parse(data.slice((lastPos + 1) | 0, (i + 1) | 0)))
+      // Reset the depth
+      depth = 0 | 0;
+      fdepth = 0 | 0;
+      // Set new lastPos
+      i++
+      lastPos = i | 0;
     }
-    if (pos === 1 && chunk === "]") {
-      if (foundDepth === depth) {
-        console.log("Got an array!", data.slice(lastPos, i + 1));
-        result.push(parse(data.slice(lastPos, i + 1)));
-        pos = 0;
-        lastPos = i + 2;
-      }
-    }
-    if (chunk === "," && pos === 0) {
-      const a = data.slice(lastPos, i)
-      if (a) {
-        console.log("General Chunk: ", data.slice(lastPos, i));
-        result.push(parse(a));
-        lastPos = i + 1;
-      }
+    if (instr === false && char === "," && depth === 0) {
+      if (data.slice((lastPos + 1) | 0, (i) | 0) != null) result.push(parse(data.slice((lastPos + 1) | 0, i | 0)));
+      lastPos = i | 0;
     }
   }
-  const trailingChunk = data.slice(lastPos, data.length);
-  console.log('Trailing: ', trailingChunk)
-  if (trailingChunk) result.push(parse(trailingChunk));
+
+  const trailingChunk = data.slice((lastPos + 1) | 0, data.length | 0)
+  if (trailingChunk != null) result.push(parse(trailingChunk))
+  // Return the final array
   return result;
 };
 
@@ -163,6 +159,7 @@ const toObject = (data) => {
 };
 
 function parse(data) {
+  data = removeJSONWhitespace(data)
   const first = data[0];
   if (first === '"') {
     return toString(data);
@@ -177,6 +174,23 @@ function parse(data) {
   } else {
     return toNumber(data);
   }
+}
+
+function removeJSONWhitespace(data) {
+  let result = []
+  // The | 0 lets the optimizer know that this is (probably) an unsigned integer
+  let i = data.length | 0
+  // 0 = off
+  // 1 = on
+  // Numbers are faster in JS than bools
+  let char
+  while (i--) {
+    // Assign character to `char` variable.
+    // It helps to pre-define a variable.
+    char = data[i]
+    if (char != " ") result.unshift(char)
+  }
+  return result.join('')
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -2,11 +2,11 @@ const kati = require('./')
 
 const str = 'Hello World "_"'
 
-const num = 1234567890
+const num = 3.14
 
 const bool = true
 
-const arr = ["Hello", "World", [3.14,["Dinosaurs",["Are", "Awesome"]],"🦕"]]
+const arr = ["Hello", "Dinosaur", { type: 'sauropod' }, [3.14, ["Dinosaurs", ["Are", "Awesome"]], "🦕"]]
 
 const obj = {
   hello: 'world',
@@ -17,19 +17,19 @@ const obj = {
 
 console.log(`Encode String: `, kati.stringify(str))
 
-console.log(`Decode String: `, kati.parse(kati.stringify(str)))
+console.log(`Decode String: `, kati.parse(`"Hello World \"_\""`))
 
 console.log(`Encode Number: `, kati.stringify(num))
 
-console.log(`Decode Number: `, kati.parse(kati.stringify(num)))
+console.log(`Decode Number: `, kati.parse(`3.14`))
 
 console.log(`Encode Boolean `, kati.stringify(bool))
 
-console.log(`Decode Boolean `, kati.parse(kati.stringify(bool)))
+console.log(`Decode Boolean `, kati.parse(`true`))
 
 console.log(`Encode Array `, kati.stringify(arr))
 
-console.log(`Decode Array `, kati.parse(kati.stringify(arr)))
+console.log(`Decode Array `, kati.parse(`["Hello","Dinosaur",{"type":"sauropod"},[3.14,["Dinosaurs",["Are","Awesome"]],"🦕"]]`))
 
 console.log(`Encode Object `, kati.stringify(obj))
 
